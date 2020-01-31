abd89918a04c1eaeac891c1d27528c3aff4eaa47
Enrique Montemayor <eemontemayor@live.com>
2020-01-30 18:11:33 -0600

    x

--- src/user/user-service.js
+++ src/user/user-service.js
@@ -58,14 +58,17 @@ const UserService = {
         .first()
 
       const languageWords = [
-        ['entraine toi', 'practice', 2],
-        ['bonjour', 'hello', 3],
-        ['maison', 'house', 4],
-        ['développeur', 'developer', 5],
-        ['traduire', 'translate', 6],
-        ['incroyable', 'amazing', 7],
-        ['chien', 'dog', 8],
-        ['chat', 'cat', null],
+        ['elefante', 'elephant', 2],
+        ['perro', 'dog', 3],
+        ['gato', 'cat', 4],
+        ['mano', 'hand',  5],
+        ['sol', 'sun',6],
+        ['cafe', 'coffee',7],
+        ['plato', 'plate',8],
+        ['bola', 'ball', 9],
+        ['home', 'casa', 10],
+        ['calle', 'street', null]
+        
       ]
 
       const [languageHeadId] = await trx
